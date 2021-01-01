24740ebe439bb6484b81814088446981dc27b869
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2021-01-01 16:56:10 -0500

    Initial Commit

---
+++ Swift UI Whatsapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Whatsapp
+//
+//  Created by Joan Cabezas on 1/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

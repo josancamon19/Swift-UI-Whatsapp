42238225939945979feef724e6d12cf62493812f
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2021-01-02 01:30:14 -0500

    Status Tab base, calls tab completed

--- Swift UI Whatsapp/Tabs/CallsView.swift
+++ Swift UI Whatsapp/Tabs/CallsView.swift
@@ -9,11 +9,57 @@ import SwiftUI
 
 struct CallsView: View {
     var body: some View {
-        VStack {
-            Text("Calls View")
+        NavigationView {
+            VStack {
+                List(0..<10) { item in
+                    
+                    HStack {
+                        HStack {
+                            Image("photo")
+                                .resizable()
+                                .frame(width: 32, height:32)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text("+57 350 4342262")
+                                    .font(.subheadline)
+                                    .foregroundColor(.red)
+                                HStack(spacing:4) {
+                                    Image(systemName: "iphone")
+                                        .resizable()
+                                        .frame(width: 4, height: 8)
+                                    Text("Missed")
+                                        .font(.caption2)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                        }
+                        Spacer()
+                        HStack {
+                            Text("17/12/20")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                            Image(systemName: "info")
+                                .foregroundColor(.blue)
+                        }.padding(.trailing, 8)
+                    }.frame(height:20)
+                    
+                }
+                .listStyle(SidebarListStyle())
+                .padding(-14)
+                
+            }.navigationBarTitle("Status")
         }.tabItem {
             Image(systemName: "iphone")
             Text("Calls")
-            
-        }    }
+        }
+        
+    }
 }
+
+struct CallsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CallsView()
+    }
+}
+
--- Swift UI Whatsapp/Tabs/StatusView.swift
+++ Swift UI Whatsapp/Tabs/StatusView.swift
@@ -7,11 +7,39 @@
 
 import SwiftUI
 
+struct PersonView: View {
+    var body: some View {
+        HStack(alignment:.top){
+            Image("astronaut")
+                .resizable()
+                .frame( width:40, height:40)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
+            VStack(alignment:.leading) {
+                Text("Joan Cabezas")
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                Text("6 hours ago")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+            }
+        }
+        
+    }
+}
+
 struct StatusView: View {
+    
     var body: some View {
         NavigationView {
-            VStack {
-                Text("Status View")
+            VStack(alignment: .leading) {
+                PersonView().padding(16)
+                
+                List(0..<10) { item in
+                    PersonView()
+                }
+                .listStyle(SidebarListStyle())
+                .padding(-14)
             }
             .navigationBarTitle("Status")
         }
